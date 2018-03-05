5e68075aee1c5e34572850fce5bdf8fe557ba11b
Alexander Kurakov <zmshurik@yandex.ru>
2018-03-05 20:27:05 +0700

    add flash

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laracasts\Flash\FlashServiceProvider::class,
         /*
          * Application Service Providers...
          */
