2050a4e4c22d4d6702af9157098a5eed7b363659
Alexander Kurakov <zmshurik@yandex.ru>
2018-03-18 21:32:33 +0700

    change card name

--- resources/views/status/edit.blade.php
+++ resources/views/status/edit.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Task statuses</div>
+                <div class="card-header">Change status</div>
 
                 <div class="card-body">
                     <form action=" {{ route('taskStatuses.update', ['id' => $taskStatus->id]) }}" method="post">
