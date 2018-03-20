220e3407cd4943a10f139d3d8863c95453cfc757
Alexander Kurakov <zmshurik@yandex.ru>
2018-03-20 19:36:52 +0700

    change home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,22 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <p class="display-4 text-center">Hello {{ Auth::user()->name }}!</p>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            Tasks created by You
+                            <span class='badge badge-primary'>{{ Auth::user()->createdTasks->count() }}</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            Tasks assigned to You
+                            <span class='badge badge-primary'>{{ Auth::user()->assignedTasks->count() }}</span>
+                        </li>                        
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            New tasks assigned to you
+                            <span class='badge badge-primary'>{{ Auth::user()->assignedTasks->where('status_id', 1)->count() }}</span>
+                        </li>
+                    </ul>                    
+                    <a class="btn btn-outline-info btn-sm" href="{{ route('tasks.index') }}">Check it</a>
                 </div>
             </div>
         </div>
