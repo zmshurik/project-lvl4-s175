48f0fb3347a8f132b5a1296da6c05f7ad5ed009e
Alexander Kurakov <zmshurik@yandex.ru>
2018-03-22 21:06:51 +0700

    fix view

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,37 +5,66 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-11">
             <div class="card">
                 <div class="card-header">Tasks</div>
 
                 <div class="card-body">
-                    <div class="text-center">@include('flash::message')</div>
-                    <form action="#" method="post">
+                    <form class="mb-2" action="{{ route('filter.store') }}" method="post">
                     @csrf
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control{{ $errors->has('statusName') ? ' is-invalid' : '' }}"
-                             placeholder="Filter will be here" name="statusName" value="{{ old('statusName') }}" required>
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-success" type="button">Add</button>
+                        <div class="form-row">
+                            <div class="form-group col-md-5">
+                                <label>Status</label>
+                                <select class="custom-select" name="statusId">
+                                    <option value="" selected>Choose...</option>
+                                    @foreach ($statuses as $status)
+                                        <option value="{{ $status->id }}">{{ $status->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-md-5">
+                                <label>Assigned user</label>
+                                <select class="custom-select" name="assignedToId">
+                                    <option value="" selected>Choose...</option>
+                                    @foreach ($users as $user)
+                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Tag</label>
+                                <select class="custom-select" name="tagId">
+                                    <option value="" selected>Choose...</option>
+                                    @foreach ($tags as $tag)
+                                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-                        @if ($errors->has('statusName'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('statusName') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-9">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="isMyTask">
+                                    <label class="form-check-label">Created by me</label>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-3 d-flex justify-content-around">
+                                <button type="submit" class="btn btn-outline-success">Apply filter</button>
+                                <a class="btn btn-outline-success" href="{{ route('tasks.index') }}">Reset</a>
+                            </div>
+                        </div>
                     </form>
+                    <div class="text-center">@include('flash::message')</div>
                     <a class="btn btn-light mb-1" href="{{ route('tasks.create') }}">Create new task</a>
                     <table class="table table-bordered table-sm">
-                        <thead class="thead-light">
+                        <thead class="thead-light text-center">
                             <tr>
                                 <th scope="col">Name</th>
                                 <th scope="col">Creator</th>
                                 <th scope="col">Assigned to</th>
                                 <th scope="col">Status</th>
                                 <th scope="col">Tags</th>
-                                <th scope="col">Actions</th>
+                                <th class="col-md-1" scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
